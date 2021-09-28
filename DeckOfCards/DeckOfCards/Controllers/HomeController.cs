84d7d53fd90273bcc4ae31378d239dfce2615097
Gusain, Nidhi <NidhiGusain@quickenloans.com>
2021-09-27 22:54:19 -0400

    checking in day 1 work

---
+++ DeckOfCards/DeckOfCards/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using DeckOfCards.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DeckOfCards.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            string domain = "https://deckofcardsapi.com";
+            string path = "/api/deck/new";
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(domain);
+            var connection = await client.GetAsync(path);// Async means our function will pause and wait until GetAsync finishes. 
+            //Then come back and pick up where we left off.
+            DeckResponse result = await connection.Content.ReadAsAsync<DeckResponse>();
+
+            return View(result);
+        }
+
+     
+
+        public async Task<IActionResult> DrawCard(string deckId)
+        {
+            string domain = "https://deckofcardsapi.com";
+            string path = $"/api/deck/{deckId}/draw/?count=5";
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(domain);
+            
+            var connection = await client.GetAsync(path);// Async means our function will pause and wait until GetAsync finishes. 
+            //Then come back and pick up where we left off.
+           
+            DrawCardResponse result = await connection.Content.ReadAsAsync<DrawCardResponse>();
+
+            return View(result);
+        }
+
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DeckOfCards/DeckOfCards/Models/DeckOfCards.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeckOfCards.Models
+{
+
+
+    public class DeckResponse
+    {
+        public bool Success { get; set; }
+        public string Deck_id { get; set; }
+        public int Remaining { get; set; }
+        public bool Shuffled { get; set; }
+    }
+
+    public class DrawCardResponse
+    {
+        public bool Success { get; set; }
+        public string Deck_id { get; set; }
+        public Card[] Cards { get; set; }
+        public int Remaining { get; set; }
+    }
+
+    public class Card
+    {
+        public string Code { get; set; }
+        public string Image { get; set; }
+        public Images Images { get; set; }
+        public string Value { get; set; }
+        public string Suit { get; set; }
+    }
+
+    public class Images
+    {
+        public string Svg { get; set; }
+        public string Png { get; set; }
+    }
+
+ 
+}
